fc02c233113f4b55ebe5c0a69a98c5f34d9e1522
runninbear5 <blakelieber@gmail.com>
2017-11-30 22:21:23 -0800

    modified adding teams

--- routes/apiGet.js
+++ routes/apiGet.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var teams;
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 //Connect to database
-mongoose.connect('mongodb://test:test@ds121456.mlab.com:21456/lieberlerts');
+mongoose.connect('mongodb://test:test@ds121456.mlab.com:21456/lieberlerts',{
+  useMongoClient: true
+});
 
 //create a schema- this is like a blueprint for data
 var teamSchema = new mongoose.Schema({
+  _id: String,
   city: String,
   country: String,
   key: String,
@@ -18,20 +20,37 @@ var teamSchema = new mongoose.Schema({
   team_number: String
 });
 var Teams = mongoose.model('Teams', teamSchema);
-
 router.post('/', function(req, res, next) {
-  teams = req.body;
-  console.log(req.body);
-
-  for(var i=0; i<500; i++){
-    var newTeams = Todo(req.body[i]).save(function(err,data){
-      if(err) throw err;
-    //  res.json(data);
+  // Teams.find({_id: "frc120"}, function(err, data){
+  //   console.log(data[0]._id);
+  // });
+  var count = Object.keys(req.body).length;
+  var i = 0;
+  while(i<count){
+    var team = new Teams({
+      _id: req.body[i].key,
+      city: req.body[i].city,
+      country: req.body[i].country,
+      key: req.body[i].key,
+      name: req.body[i].name,
+      nickname: req.body[i].nickname,
+      state_prov: req.body[i].state_prov,
+      team_number: req.body[i].team_number
     });
-  }
-  res.send(req.body);
+      Teams.find({_id: req.body[i].key}).remove(function(err, data){
+        if(err) throw err;
+      });
+      team.save(function(err){
+        if(err) throw err;
+      });
+      i++;
+    }
+  res.send("good");
 });
 router.get('/', function(req, res){
-  res.send(teams);
+  Teams.find({_id: "frc121"}, function(err, data){
+    if(err) throw err;
+    res.send(data[0].nickname);
+  });
 });
 module.exports = router;
