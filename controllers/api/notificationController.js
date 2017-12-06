fb2d53ff7cd12a3fb2d23ee2bbaa66e2827c4111
runninbear5 <blakelieber@gmail.com>
2017-12-05 21:55:57 -0800

    Added match_score notification

--- controllers/api/notificationController.js
+++ controllers/api/notificationController.js
@@ -3,81 +3,93 @@ var sender = require('../emailSender')
 exports.tbaNotify = function(req, res){
   var msg = req.body;
   if(msg.message_type === "verification"){
-    sender.send(msg.message_data.verification_key, 'blakelieber@gmail.com');
+    verification(msg);
   }
   else if(msg.message_type === "upcoming_match"){
-    var email = "The teams are: ";
-    var teams = msg.message_data.team_keys;
-    var teamList = [];
-    teams.forEach(function(team){
-      email += team + ", ";
-      teamList.push(team);
-    });
-    var time = "";
-    var currentMilli = msg.message_data.scheduled_time;
-    var seconds =  ((currentMilli/1000) % 60);
-    var add0Min = "0";
-    var add0Sec = "0";
-    var minuites = ((currentMilli/(1000*60)) % 60);
-    var hours =  ((currentMilli/(1000*60*60)) % 12);
-    seconds = Math.round(seconds);
-    minuites = Math.round(minuites);
-    hours = Math.round(hours);
-    if((currentMilli/(1000*60*60)) % 24 > 12){
-        time = "am";
-    }else{
-        time = "pm";
-    }
-    if(((currentMilli/(1000*60)) % 60) < 10){
-       add0Min = "0";
-    }else{
-        add0Min = "";
-    }
-    if(((currentMilli/(1000)) % 60) < 10){
-       add0Sec = "0";
-    }else{
-        add0Sec = "";
-    }
-    email += "\nThe event is at " + msg.message_data.event_name+".\nThe event is scheduled for "+hours+":"+add0Min+minuites+":"+add0Sec+seconds+time;
-    sender.send(email, 'blakelieber@gmail.com');
+    upcomingMatch(msg);
   }else if(msg.message_type === "match_score"){
-    var winningTeam = "";
-    var redScore = msg.message_data.match.alliances.red.score;
-    var blueScore = msg.message_data.match.alliances.blue.score;
-    var email = "The event name is " +msg.message_data.event_name+" and the match number is "+msg.message_data.match.match_number+".\n";
-    if(blueScore > redScore){
-      email += "The blue team won with a score of " +blueScore+ " to " +redScore+".\n";
-    }else if(redScore > blueScore){
-      email += "The red team won with a score of " +redScore+ " to " +blueScore+".\n";
-    }else{
-      email += "The teams tied with a score of " +redScore+".\n";
-    }
-    email += "The teams on blue teams are ";
-    var teams = msg.message_data.match.alliances.blue.teams;
-    var teamList = [];
-    teams.forEach(function(team){
-      teamList.push(team);
-    });
-    teams = msg.message_data.match.alliances.red.teams;
-    teams.forEach(function(team){
-      teamList.push(team);
-    });
-    for(var i=0; i<3; i++){
-      if(i<2){
-        email += teamList[i] + ", ";
-      }else if(i===2){
-        email += teamList[i] +".\n";
-      }
-    }
-    email += "The teams on red team are ";
-    for(var i=3; i<6; i++){
-      if(i<5){
-        email += teamList[i] + ", ";
-      }else if(i===5){
-        email += teamList[i] +".\n";
-      }
-    }
-    sender.send(email, "blakelieber@gmail.com")
-    res.send("good");
+    matchScore(msg);
   }
+  res.send("good");
+};
+
+var verification = function(msg){
+  sender.send(msg.message_data.verification_key, 'blakelieber@gmail.com');
+};
+
+var upcomingMatch = function(msg){
+  var email = "The teams are: ";
+  var teams = msg.message_data.team_keys;
+  var teamList = [];
+  teams.forEach(function(team){
+    email += team + ", ";
+    teamList.push(team);
+  });
+  var time = "";
+  var currentMilli = msg.message_data.scheduled_time;
+  var seconds =  ((currentMilli/1000) % 60);
+  var add0Min = "0";
+  var add0Sec = "0";
+  var minuites = ((currentMilli/(1000*60)) % 60);
+  var hours =  ((currentMilli/(1000*60*60)) % 12);
+  seconds = Math.round(seconds);
+  minuites = Math.round(minuites);
+  hours = Math.round(hours);
+  if((currentMilli/(1000*60*60)) % 24 > 12){
+      time = "am";
+  }else{
+      time = "pm";
+  }
+  if(((currentMilli/(1000*60)) % 60) < 10){
+     add0Min = "0";
+  }else{
+      add0Min = "";
+  }
+  if(((currentMilli/(1000)) % 60) < 10){
+     add0Sec = "0";
+  }else{
+      add0Sec = "";
+  }
+  email += "\nThe event is at " + msg.message_data.event_name+".\nThe event is scheduled for "+hours+":"+add0Min+minuites+":"+add0Sec+seconds+time;
+  sender.send(email, 'blakelieber@gmail.com');
+};
+
+var matchScore = function(msg){
+  var winningTeam = "";
+  var redScore = msg.message_data.match.alliances.red.score;
+  var blueScore = msg.message_data.match.alliances.blue.score;
+  var email = "The event name is " +msg.message_data.event_name+" and the match number is "+msg.message_data.match.match_number+".\n";
+  if(blueScore > redScore){
+    email += "The blue team won with a score of " +blueScore+ " to " +redScore+".\n";
+  }else if(redScore > blueScore){
+    email += "The red team won with a score of " +redScore+ " to " +blueScore+".\n";
+  }else{
+    email += "The teams tied with a score of " +redScore+".\n";
+  }
+  email += "The teams on blue teams are ";
+  var teams = msg.message_data.match.alliances.blue.teams;
+  var teamList = [];
+  teams.forEach(function(team){
+    teamList.push(team);
+  });
+  teams = msg.message_data.match.alliances.red.teams;
+  teams.forEach(function(team){
+    teamList.push(team);
+  });
+  for(var i=0; i<3; i++){
+    if(i<2){
+      email += teamList[i] + ", ";
+    }else if(i===2){
+      email += teamList[i] +".\n";
+    }
+  }
+  email += "The teams on red team are ";
+  for(var i=3; i<6; i++){
+    if(i<5){
+      email += teamList[i] + ", ";
+    }else if(i===5){
+      email += teamList[i] +".\n";
+    }
+  }
+  sender.send(email, "blakelieber@gmail.com")
 };
