b8dce8468b36ceb49d83012c62a37e3b83eb1d77
runninbear5 <blakelieber@gmail.com>
2017-12-17 17:57:52 -0800

    Issues with passport

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
 
-// var auth = require(''./routes/auth');
 var home = require('./routes/home');
 var users = require('./routes/users');
 //var apiGet = require('./routes/apiGet');
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', home);
 app.use('/users', users);
 app.use('/api', api);
-app.use('/auth', auth)
+app.use('/auth', auth);
 //app.use('/auth', api);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,20 +1,9 @@
 var passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
-passport.use(new GoogleStrategy({
-    clientID: "826465670781-51ktnva0pfdd69h2v5thekp4qn8uv4us.apps.googleusercontent.com",
-    clientSecret: "uwtOvjdRxVkq4cXWoU9pBbk8",
-    callbackURL: "http://localhost:3000/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-       User.findOrCreate({ googleId: profile.id }, function (err, user) {
-         return done(err, user);
-       });
-  }
-));
-exports.authenticate = passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] });
+exports.authenticate = (passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
 
 exports.callback = (passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
-   res.redirect('/');
+  //  console.log(req);
+    res.redirect('/');
  });
