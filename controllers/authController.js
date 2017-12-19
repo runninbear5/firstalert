24b96b75b8a568fc0d9f027d113540a69276ccd1
runninbear5 <blakelieber@gmail.com>
2017-12-18 21:16:46 -0800

    fixed .gitignore

--- controllers/authController.js
+++
@@ -1,9 +0,0 @@
-var passport = require('passport');
-
-exports.authenticate = (passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
-
-exports.callback = (passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-  //  console.log(req);
-    res.redirect('/');
- });
