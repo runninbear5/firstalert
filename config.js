d68f013ca7e6eb3acb150b7e5a394f9220064f25
runninbear5 <blakelieber@gmail.com>
2017-12-18 20:33:25 -0800

    git issues

---
+++ config.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+
+const env = process.env.NODE_ENV;
+
+var config = {
+    db: {
+        type: 'mongodb',
+        host: 'ds161016.mlab.com:61016',
+        dbname: 'om_lieberlerts',
+        username: process.env.DB_USER,
+        password: process.env.DB_PWD
+    },
+    auth: {
+        google: {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: "http://localhost:3000/auth/google/callback"
+        }
+    },
+    tba: {
+        api_url: 'https://www.thebluealliance.com/api/v3',
+        api_key: process.env.TBA_API_KEY
+    }
+};
+
+// override production settings
+if (env == 'production') {
+    config.auth.google.callbackURL = "http://lieberlerts.herokuapp.com/auth/google/callback"
+}
+
+// export
+module.exports = config;
---
+++ controllers/adminController.js
@@ -0,0 +1,53 @@
+var request = require('request');
+var config = require('../config')
+
+var Team = require('../models/team');
+
+exports.populateTeams = function(req, res, next) {
+
+    var startPageNum = 0;
+
+    upsertTeams(startPageNum, function() {
+        res.send('Done');
+    });
+};
+
+// private methods
+function upsertTeams(page, next) {
+
+    var updateOptions = { upsert: true, new: true, setDefaultsOnInsert: true };
+
+    var url = `${config.tba.api_url}/teams/${page}`;
+
+    request(url, { headers: {'X-TBA-Auth-Key': config.tba.api_key} }, function(err, rsp, body) {
+        var teams = JSON.parse(body);
+
+        if (teams.length > 0 ) {
+            teams.forEach(team => {
+                var query = { _id: team.key };
+
+                var update = { 
+                    _id: team.key, 
+                    city: team.city, 
+                    country: team.country, 
+                    key: team.key, 
+                    name: team.name, 
+                    nickname: team.nickname,
+                    state_prov: team.state_prov, 
+                    team_number: team.team_number 
+                };
+
+                // upsert the team
+                Team.findByIdAndUpdate(query, update, updateOptions, function(error, result) {
+                    if (error) return;
+                });
+            });
+            console.log(page);
+            page += 1;
+            upsertTeams(page, next);
+        }
+        else {
+            next();
+        }
+    });
+};
