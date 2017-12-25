cf0d6f4e0cde0b5137c957e3fd769fbce6031277
runninbear5 <blakelieber@gmail.com>
2017-12-25 10:56:30 -0800

    Made webpage look nicer and added user recognition

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,9 +1,9 @@
 exports.home = function(req, res, next) {
   console.log(req.user);
-  res.render('home/index', { title: 'Express' });
+  res.render('home/index', { title: 'Express', request: req });
 };
 
 exports.login = function(req, res, next)  {
   console.log(req.user);
-  res.render('home/login', { title: 'Express'});
+  res.render('home/login', { title: 'Express', request: req});
 };
